7922a608771c7682d7ee16645d1025dbb6dc857e
RaviAnand Mohabir <moravrav@gmail.com>
2021-11-22 10:24:39 +0100

    ✨ Include markdown table formatting option

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import argparse
 
 import praw
+from praw.reddit import Submission
+
 from coingecko import *
 from reddit_comments_crypto_counter import *
+from formatting import *
 
 
-def main(reddit: praw.Reddit, url: str, top: int = 100):
-    cg_coins_list = get_cg_coins_list()
-    ranked, comments_analyzed = analyze_comments(reddit, url, cg_coins_list)
-    for rank, (ticker, count) in enumerate(ranked):
-        if rank > top - 1:
-            break
-        name = ""
-        for coin in cg_coins_list:
-            if "Binance-Peg" in coin["name"]:
-                continue
-            if ticker.lower() == coin["symbol"].lower():
-                name = coin["name"]
-                break
-        print(f"{rank + 1}. {name} ({ticker.upper()}) - {count}")
-        print()
+def main(reddit: praw.Reddit, url: str, top: int = 100, markdown: bool = False):
+    cg_coins_market = get_cg_coins_markets()
+    submission: Submission = reddit.submission(url=url)
+    ranked, comments_analyzed = analyze_comments(submission, cg_coins_market)
+    if ranked:
+        if markdown:
+            print(get_markdown_table(ranked, cg_coins_market, top))
+        else:
+            for rank, (ticker, count) in enumerate(ranked):
+                if rank > top - 1:
+                    break
+                coin: CoinMarket = get_most_popular_coin_with_ticker(ticker, cg_coins_market)
+                if not coin:
+                    print("No coin found for ticker:", ticker)
+                else:
+                    print(f"{rank + 1}. {coin['name']} ({ticker.upper()}) - {count} (Market Cap: ${coin['market_cap']:,})")
+                    print()
+    else:
+        print("No coins found in thread.")
     print(f"{comments_analyzed:,} comments analyzed.")
 
 
@@ -34,8 +41,10 @@ parser.add_argument(
 parser.add_argument("--ignore-english-words", dest="ignore_english_words", action="store_true",
                     default=True, help="Ignore English words in comments that may be detected as coins or tokens.")
 parser.add_argument("--no-ignore-english-words", dest="ignore_english_words", action="store_false")
+parser.add_argument("--markdown", "-md", dest="markdown", action="store_true",
+                    default=False, help="Enable markdown output with CoinGecko URL.")
 
 if __name__ == "__main__":
     args = parser.parse_args()
     reddit = praw.Reddit("CCC", user_agent="Reddit crypto comments ticker counter by Dan6erbond.")
-    main(reddit, args.url, args.top)
+    main(reddit, args.url, args.top, args.markdown)
