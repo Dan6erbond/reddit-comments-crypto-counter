3951010ae8d72bf34679dc7a3ddc4e0d896a8bd9
RaviAnand Mohabir <moravrav@gmail.com>
2021-11-22 10:28:12 +0100

    ♻️ Restructure code with `lib/` directory

---
+++ lib/__init__.py
@@ -0,0 +1,4 @@
+from coingecko import (CoinMarket, get_cg_coins_list, get_cg_coins_markets,
+                       get_most_popular_coin_with_ticker,
+                       get_symbols_names_dict)
+from reddit_comments_crypto_counter import analyze_comments
---
+++ lib/formatting.py
@@ -0,0 +1,24 @@
+from typing import Dict, List, Optional, Tuple, Union
+
+from coingecko import *
+
+
+def get_markdown_table(ranked: List[Tuple[str, int]],
+                       cg_coins_list: Optional[List[Union[CoinMarket, Dict]]], top: int = 100) -> str:
+    """
+    Return a markdown table of the given list of ranked crypto mentions.
+    """
+    cg_coins_list = cg_coins_list or get_cg_coins_list()
+    lines = []
+    lines.append("Nr. | Count | Name | Ticker | Market Cap (USD) | Link")
+    lines.append(":--- |----:|:----|:------:|--------------:|:----")
+    for rank, (ticker, count) in enumerate(ranked):
+        if rank > top - 1:
+            break
+        coin: CoinMarket = get_most_popular_coin_with_ticker(ticker, cg_coins_list)
+        if not coin:
+            print("No coin found for ticker:", ticker)
+        else:
+            lines.append(
+                f"{rank + 1}. | {count} | {coin['name']} | {ticker.upper()} | ${coin['market_cap']:,} | [CoinGecko ↗](https://www.coingecko.com/en/coins/{coin['id']})")
+    return "\n".join(lines)
